1c5c0c67789aa639e791b2d8f0afc35cb99aeed6
Deepesh Sengar <121319912+DeepeshSengarIO@users.noreply.github.com>
2026-02-07 19:03:01 +0100

    test: start a game and it should appear with zero score in game summary

--- src/test/java/ScoreboardTest.java
+++ src/test/java/ScoreboardTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ScoreboardTest {
@@ -7,5 +10,15 @@ public class ScoreboardTest {
     void gameSummaryIsEmptyWhenNoGameStarted(){
         Scoreboard scoreboard = new Scoreboard();
         assertTrue(scoreboard.getSummary().isEmpty());
+    }
+
+    @Test
+    void startedGameAppearsInSummaryWithZeroScore() {
+        Scoreboard scoreboard = new Scoreboard();
+        scoreboard.startGame("Mexico", "Canada", 900); // I assumed time to 900 as it started at 9:00 AM for easiness but a time producer can be injected.
+        List<MatchSummary> summary = scoreboard.getSummary();
+        assertEquals(1, summary.size());
+        assertEquals(0, summary.get(0).homeScore());
+        assertEquals(0, summary.get(0).awayScore());
     }
 }
