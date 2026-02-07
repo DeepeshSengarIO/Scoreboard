3c24f7bf07ee28f13d9e07a8d512d2027cc09da6
Deepesh Sengar <121319912+DeepeshSengarIO@users.noreply.github.com>
2026-02-07 18:56:54 +0100

    feat: implement Scoreboard class and return empty list when no game started
    Implemented empty Match Class Support
    return an empty summary when no game is started

---
+++ src/main/java/Scoreboard.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scoreboard {
+    public Scoreboard(){}
+    public List<Match> getSummary(){
+        return new ArrayList<>();
+    }
+}
